02492682b33d8d349cbec6e7737544df8af88f69
Brachamul <barnaby.brachamul@gmail.com>
2014-10-24 19:47:28 +0200

    Extra Fields

--- collector/forms.py
+++ collector/forms.py
@@ -10,13 +10,14 @@ import datetime
 class InscritForm(forms.ModelForm):
 	class Meta:
 		model = Inscrit
-		fields = ['email', 'prenom', 'nom', 'adresse_postale', 'numero_de_telephone', 'numero_adherent', 'date_de_naissance', 'photo', 'bio', 'departement', 'compte_twitter', 'compte_facebook']
+		fields = ['email', 'prenom', 'nom', 'adresse_postale', 'numero_de_telephone', 'numero_adherent', 'date_de_naissance', 'photo', 'cni', 'declaration', 'bio', 'departement', 'compte_twitter', 'compte_facebook']
 		# , 'document'
 		help_texts = {
 			'photo': 'Cette photo sera utilisée pour vous représenter dans nos supports de communication.',
 			'bio': 'Un court texte de présentation pour accompagner la photo, 500 caractères maxi.',
 			'numero_adherent': 'Si vous le connaissez...',
 			'adresse_postale': 'Numéro, rue, code postal, ville, ...'
+			'declaration': 'Vous trouverez une déclaration type <a href="https://www.facebook.com/download/1574081842813179/D%C3%A9cla%20candidature%20CN.odt" target="_blank">ici</a>.'
 		}
 		widgets = {
 			'date_de_naissance': SelectDateWidget(months=MOIS, years=range(2000, 1930, -1)),
--- collector/models.py
+++ collector/models.py
@@ -6,18 +6,25 @@ class Inscrit(models.Model):
 
 	# Rentré par l'utilisateur
 	email = models.EmailField(max_length=255, unique=True)
-	prenom = models.CharField("prénom", max_length=255)
+	prenom = models.CharField("prénom", max_length=255, null=True)
 	nom = models.CharField(max_length=255)
 	numero_adherent = models.PositiveSmallIntegerField("numéro d'adhérent", max_length=10, blank=True, null=True)
 	date_de_naissance = models.DateField()
 	adresse_postale = models.CharField(max_length=512, blank=True, null=True)
 	numero_de_telephone = models.PositiveSmallIntegerField("téléphone", max_length=12, blank=True, null=True)
-	departement = models.CharField("département", max_length=3, choices=DEPARTEMENTS)
+	departement = models.CharField("département", max_length=3, choices=DEPARTEMENTS, null=True)
 	compte_twitter = models.CharField(max_length=255, blank=True, null=True, default='@')
 	compte_facebook = models.URLField(max_length=255, blank=True, null=True, default='http://facebook.com/')
-	photo = models.ImageField(upload_to='photos', blank=True, null=True)
 	bio = models.TextField(max_length=512, blank=True, null=True)
-	document = models.FileField(upload_to='documents', blank=True, null=True)
+
+	import os
+	def get_upload_path(instance, filename):
+		return os.path.join(
+			instance.nom, "-", instance.prenom, filename)
+
+	photo = models.ImageField(upload_to=get_upload_path, blank=True, null=True)
+	cni = models.FileField("Pièce d'identité", upload_to=get_upload_path, blank=True, null=True)
+	declaration = models.FileField("Déclaration de candidature", upload_to=get_upload_path, blank=True, null=True)
 
 	# Système
 	created = models.DateTimeField(auto_now_add=True)
@@ -25,3 +32,4 @@ class Inscrit(models.Model):
 
 	def __str__(self):
 		return self.prenom + ' ' + self.nom
+
